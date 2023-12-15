9d8317c3910493fb5904d1e554bff42cd51ed79e
Peter Salomonsen <petersalomonsen@runbox.com>
2023-12-15 19:05:00 +0000

    add meteor wallet to wallet selector

--- public/webcomponentapp.js
+++ public/webcomponentapp.js
@@ -2,10 +2,11 @@ import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupModal } from "@near-wallet-selector/modal-ui-js";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
 import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 
 const selector = await setupWalletSelector({
   network: "mainnet",
-  modules: [setupMyNearWallet(), setupHereWallet()],
+  modules: [setupMyNearWallet(), setupHereWallet(), setupMeteorWallet()],
 });
 
 const modal = setupModal(selector, {
