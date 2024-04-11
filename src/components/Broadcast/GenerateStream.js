6276b6ce3c3888f222a5d4bede483c77a60d29c4
stefano <stefano1990@gmail.com>
2024-04-11 12:46:44 +0200

    add name prop to stream

--- src/components/Broadcast/GenerateStream.js
+++ src/components/Broadcast/GenerateStream.js
@@ -1,39 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStore } from "./state";
 
 const GenerateStream = () => {
-	const { setStreamKey } = useStore();
+  const { setStreamKey } = useStore();
 
-	const createStream = async () => {
-		try {
-			const response = await fetch("https://livepeer.studio/api/stream", {
-				method: "POST",
-				headers: {
-					Authorization: `Bearer ${process.env.REACT_APP_LIVEPEER_STUDIO_API_KEY}`,
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					name: "test_stream",
-				}),
-			});
+  const [name, setName] = useState("");
 
-			if (!response.ok) {
-				throw new Error("Network response was not ok");
-			}
+  const createStream = async (event) => {
+    event.preventDefault();
 
-			const data = await response.json();
-			console.log("Stream created:", data);
-			setStreamKey(data.streamKey);
-		} catch (error) {
-			console.error("Error creating stream:", error);
-		}
-	};
+    try {
+      const response = await fetch("https://livepeer.studio/api/stream", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_LIVEPEER_STUDIO_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+        }),
+      });
 
-	return (
-		<button type="button" onClick={createStream}>
-			create stream
-		</button>
-	);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log("Stream created:", data);
+      setStreamKey(data.streamKey);
+    } catch (error) {
+      console.error("Error creating stream:", error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={createStream}>
+        <label>
+          Stream name:
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <button type="submit">Start stream</button>
+      </form>
+    </>
+  );
 };
 
 export default GenerateStream;
