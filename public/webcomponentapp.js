2c82b21d1acdf166c275d1dfa8ae274d3e0899b9
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-05-12 18:20:20 -0400

    revert wallet selector change

--- public/webcomponentapp.js
+++ public/webcomponentapp.js
@@ -5,53 +5,15 @@ import { setupHereWallet } from "@near-wallet-selector/here-wallet";
 import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import { setupSender } from "@near-wallet-selector/sender";
 
-const getNetworkPreset = (networkId) => {
-  switch (networkId) {
-    case "mainnet":
-      return {
-        networkId,
-        nodeUrl: "https://rpc.mainnet.near.org",
-        helperUrl: "https://helper.mainnet.near.org",
-        explorerUrl: "https://nearblocks.io",
-        indexerUrl: "https://api.kitwallet.app",
-      };
-    case "testnet":
-      return {
-        networkId,
-        nodeUrl: "https://rpc.testnet.near.org",
-        helperUrl: "https://helper.testnet.near.org",
-        explorerUrl: "https://testnet.nearblocks.io",
-        indexerUrl: "https://testnet-api.kitwallet.app",
-      };
-    default:
-      throw Error(`Failed to find config for: '${networkId}'`);
-  }
-};
-
-const viewer = document.querySelector("near-social-viewer");
-
-const rpc = viewer.getAttribute("rpc");
-const networkConfig = getNetworkPreset("mainnet");
-
-if (rpc) {
-  networkConfig.nodeUrl = rpc;
-}
-
 const selector = await setupWalletSelector({
-  network: networkConfig,
-  modules: [
-    setupMyNearWallet(),
-    setupHereWallet(),
-    setupMeteorWallet(),
-    setupSender(),
-  ],
+  network: "mainnet",
+  modules: [setupMyNearWallet(), setupHereWallet(), setupMeteorWallet(), setupSender()],
 });
 
 const modal = setupModal(selector, {
   contractId: "social.near",
 });
-document
-  .getElementById("open-walletselector-button")
-  .addEventListener("click", () => modal.show());
 
+document.getElementById('open-walletselector-button').addEventListener('click', () => modal.show());
+const viewer = document.querySelector('near-social-viewer');
 viewer.selector = selector;
