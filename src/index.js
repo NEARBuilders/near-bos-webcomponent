68b09a8989d64ed70482f8424d01de4e5b1c5153
stefano <stefano1990@gmail.com>
2024-06-22 14:48:29 +0200

    Revert style changes

--- src/index.js
+++ src/index.js
@@ -4,58 +4,46 @@ import "./index.css";
 import App from "./App";
 
 class NearSocialViewerElement extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `<slot></slot>`;
-    this.selectorPromise = new Promise(
-      (resolve) => (this.selectorPromiseResolve = resolve)
-    );
-    this.reactRoot = null;
-  }
-
-  set selector(selector) {
-    this.selectorPromiseResolve(selector);
-  }
-
-  connectedCallback() {
-    const container = document.createElement("div");
-    this.appendChild(container);
-
-    this.reactRoot = createRoot(container);
-    this.renderRoot();
-  }
-
-  static get observedAttributes() {
-    return ["src", "code", "initialprops", "rpc", "network", "enablehotreload"];
-  }
-
-  renderRoot() {
-    const src = this.getAttribute("src");
-    const code = this.getAttribute("code");
-    const initialProps = this.getAttribute("initialprops");
-    const rpc = this.getAttribute("rpc");
-    const network = this.getAttribute("network");
-    const enableHotReload = this.hasAttribute("enablehotreload");
-
-    this.reactRoot.render(
-      <App
-        src={src}
-        code={code}
-        initialProps={JSON.parse(initialProps)}
-        rpc={rpc}
-        network={network}
-        selectorPromise={this.selectorPromise}
-        enableHotReload={enableHotReload}
-      />
-    );
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue !== newValue) {
-      this.renderRoot();
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML = `<slot></slot>`;
+        this.selectorPromise = new Promise(resolve => this.selectorPromiseResolve = resolve);
+        this.reactRoot = null;
     }
-  }
+
+    set selector(selector) {
+        this.selectorPromiseResolve(selector);
+    }
+
+    connectedCallback() {
+        const container = document.createElement('div');
+        this.appendChild(container);
+
+        this.reactRoot = createRoot(container);
+        this.renderRoot();
+    }
+
+    static get observedAttributes() {
+        return ['src', 'code', 'initialprops', 'rpc', 'network', 'enablehotreload'];
+    }
+
+    renderRoot() {
+        const src = this.getAttribute('src');
+        const code = this.getAttribute('code');
+        const initialProps = this.getAttribute('initialprops');
+        const rpc = this.getAttribute('rpc');
+        const network = this.getAttribute('network');
+    		const enableHotReload = this.hasAttribute("enablehotreload");
+
+        this.reactRoot.render(<App src={src} code={code} initialProps={JSON.parse(initialProps)} rpc={rpc} network={network} selectorPromise={this.selectorPromise} enableHotReload={enableHotReload}/>);
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            this.renderRoot();
+        }
+    }
 }
 
-customElements.define("near-social-viewer", NearSocialViewerElement);
+customElements.define('near-social-viewer', NearSocialViewerElement);
