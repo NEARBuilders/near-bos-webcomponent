8f2dff5c070734180f8b8a6ee1ef9fea50719a98
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-07-09 10:56:07 -0400

    Patches Link and adds tests (#32)
    * patches Link and adds tests
    
    * poc: pass customElements
    
    * Revert "poc: pass customElements"
    
    This reverts commit 2e65ec40ab0e7911e0799de2973f43a2fe4e9a41.
    
    * adds code tests

--- playwright-tests/tests/vm/customelements.spec.js
+++ playwright-tests/tests/vm/customelements.spec.js
@@ -1,7 +1,8 @@
-import { expect, test } from "@playwright/test";
+import { describe, expect, test } from "@playwright/test";
 import {
   escapeHtml,
   pauseIfVideoRecording,
+  useCode,
   waitForSelectorToBeVisible,
 } from "../../testUtils";
 
@@ -34,20 +35,12 @@ test("should throw error if a custom element is referenced from code, but not de
 test("should render custom element when defined, with attributes and children", async ({
   page,
 }) => {
-  // Navigate to the default route
   await page.goto("/");
 
-  const code = escapeHtml('return (<Link to="/anywhere">go somewhere</Link>);');
-
-  // Set code attribute to reference an existing custom element
-  await page.evaluate((code) => {
-    document.body.innerHTML = `
-    <near-social-viewer code="${code}" />
-    `;
-  }, code);
-
-  // Verify the viewer is visible
-  await waitForSelectorToBeVisible(page, "near-social-viewer");
+  await useCode(page, "custom/link.js", {
+    to: "/anywhere",
+    text: "go somewhere",
+  });
 
   // Expect the custom element to be rendered
   const renderedLink = await page.getByRole("link");
